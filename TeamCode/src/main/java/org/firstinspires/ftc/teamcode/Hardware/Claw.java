12bb41b20aaf49e43d4450c2f080fc34afae13dd
sophiazhang14 <88739834+sophiazhang14@users.noreply.github.com>
2022-09-07 16:15:32 -0500

    adds

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Claw.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.Hardware;
+
+public class Claw {
+    //Shriya: clas
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Lift.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.Hardware;
+
+public class Lift {
+    //Krish: lift
+}
