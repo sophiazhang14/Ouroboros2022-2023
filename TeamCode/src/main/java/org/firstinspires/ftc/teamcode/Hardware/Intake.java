ccd9bea0e31ac9ff5da1f41d980ab85d810e8b67
ilovemtb1 <112122009+ilovemtb1@users.noreply.github.com>
2022-09-07 17:06:03 -0500

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Intake.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.Hardware;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 public class Intake {
 
 
