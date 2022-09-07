b46567e462c78c2bf20249b33290754a95666394
sophiazhang14 <88739834+sophiazhang14@users.noreply.github.com>
2022-09-07 16:36:23 -0500

    adds

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/DriveTrain.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+public class DriveTrain {
+    //sophia: encoder move
+    public DriveTrain(){
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Telelib.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Telelib.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.Hardware;
 
 public class Telelib {
-
+    // hannah: intake
+    // krish: lift
+    // shriya: claw
 }
