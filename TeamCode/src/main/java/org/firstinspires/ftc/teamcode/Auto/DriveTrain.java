5095d4c9247ab7d163a8cd60d9f28b085cb93f9a
sophiazhang14 <88739834+sophiazhang14@users.noreply.github.com>
2022-09-07 21:43:48 -0500

    sophia - drive train (encoder move, kill, etc)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/DriveTrain.java
@@ -1,8 +1,71 @@
 package org.firstinspires.ftc.teamcode.Auto;
 
-public class DriveTrain {
-    //sophia: encoder move
-    public DriveTrain(){
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+public class DriveTrain {
+    //sophia
+    public DcMotor br;
+    public DcMotor bl;
+    public DcMotor fr;
+    public DcMotor fl;
+
+    public LinearOpMode opMode;
+
+    public DriveTrain(){
+        this.opMode = opMode;
+
+        br = opMode.hardwareMap.dcMotor.get("br");
+        bl = opMode.hardwareMap.dcMotor.get("bl");
+        fr = opMode.hardwareMap.dcMotor.get("fr");
+        fl = opMode.hardwareMap.dcMotor.get("fl");
+
+        // how to know what direction and power to reset to? - sophia
+    }
+    public void resetEncoders(){
+        //how to set power and direction? - sophia
+    }
+
+    public void encoderMove(double power, double distance, boolean direction, double runtime){
+
+        ElapsedTime time = new ElapsedTime();
+
+        // reset encoders, explain what happen if don't - sophia
+        resetEncoders();
+        time.reset();
+
+        double dis = 0;
+
+        while (time.seconds() < runtime && opMode.opModeIsActive() && dis < distance * (Math.PI/90)){
+            dis = (bl.getCurrentPosition() + br.getCurrentPosition() + fl.getCurrentPosition() + fr.getCurrentPosition())/4;
+            if (!opMode.opModeIsActive())    {
+                kill();
+            }
+            if (direction) {
+                br.setPower(power);
+                bl.setPower(power);
+                fr.setPower(power);
+                fl.setPower(power);
+            }
+            else{
+                br.setPower(-power);
+                bl.setPower(-power);
+                fr.setPower(-power);
+                fl.setPower(-power);
+            }
+        }
+        kill();
+    }
+
+    public void turn(){
+
+    }
+
+    public void kill(){
+        br.setPower(0);
+        bl.setPower(0);
+        fr.setPower(0);
+        fl.setPower(0);
     }
 }
