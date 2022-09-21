55aaf79bf51b281445ab16eaf9871a6b01fb20ff
sophiazhang14 <88739834+sophiazhang14@users.noreply.github.com>
2022-09-21 16:50:01 -0500

    Sophia needs PID code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Turret.java
@@ -1,4 +1,27 @@
 package org.firstinspires.ftc.teamcode.Hardware;
 
 public class Turret {
+    public void turnPID (double goal, boolean isRight, double kP, double kI, double kD, double timeOutMS) {
+
+        runtime.reset();
+        sensors.angles = sensors.gyro.getAngularOrientation();
+
+        while (opMode.opModeIsActive() && runtime.milliseconds() <= timeOutMS && goal - sensors.getGyroYaw() > 1 ) {
+
+            sensors.angles = sensors.gyro.getAngularOrientation();
+
+            error = goal - sensors.getGyroYaw();
+            proportional = error * kP;
+            time = runtime.milliseconds();
+            integral += ((time - prevTime) * error) * kI;
+            derive = ((error - prevError) / (time - prevTime)) * kD;
+            power = proportional + integral + derive;
+
+            turn(power, isRight);
+
+            prevTime = runtime.milliseconds();
+            prevError = goal - sensors.getGyroYaw();
+
+
+        }
 }
