460dc054766790ec7ac77c204fb80f0c082b332b
ilovemtb1 <112122009+ilovemtb1@users.noreply.github.com>
2022-10-28 17:20:50 -0500

    hannah: 10/28/encoder auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/BlueRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/BlueRight.java
@@ -1,4 +1,22 @@
 package org.firstinspires.ftc.teamcode.Auto;
 
-public class BlueRight {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+
+public class BlueRight extends LinearOpMode {
+    public void runOpMode() throws InterruptedException {
+       DriveTrain dt = new DriveTrain(this);
+       Vision vis = new Vision(this);
+
+       if(vis.senseBlue() == 1){
+            dt.encoderMove(0.5,30, 5, false, false );
+    }
+        else if(vis.senseBlue() ==2 ){
+        dt.encoderMove(.5, 50, 3, false, false);
+    }  else{
+            dt.encoderMove(.5, 70, 3, false, false);
+        }
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/DriveTrain.java
@@ -13,7 +13,7 @@ public class DriveTrain {
 
     public LinearOpMode opMode;
 
-    public DriveTrain(){
+    public DriveTrain(LinearOpMode opMode){
         this.opMode = opMode;
 
         br = opMode.hardwareMap.dcMotor.get("br");
@@ -26,7 +26,7 @@ public class DriveTrain {
     public void resetEncoders(){
         //how to set power and direction? - sophia
     }
-    public void encoderMove(double power, double distance, boolean direction, double runtime){
+    public void encoderMove(double power, double distance, double runtime, boolean strafe, boolean strafeRight){
 
         ElapsedTime time = new ElapsedTime();
 
@@ -41,20 +41,28 @@ public class DriveTrain {
             if (!opMode.opModeIsActive())    {
                 kill();
             }
-            if (direction) {
+            if (!strafe) {
                 br.setPower(power);
                 bl.setPower(power);
                 fr.setPower(power);
                 fl.setPower(power);
             }
-            else{
-                br.setPower(-power);
+            else if(strafeRight){
+                br.setPower(power);
                 bl.setPower(-power);
                 fr.setPower(-power);
+                fl.setPower(power);
+            } else {
+                br.setPower(-power);
+                bl.setPower(power);
+                fr.setPower(power);
                 fl.setPower(-power);
             }
         }
         kill();
+    }
+    public void turnPID(){
+
     }
 
     public void turn(){
